85e2eb0b67410fce52591cd1513067a6e072a988
Sviridov <SvyrydovArtem@gmail.com>
2017-04-29 19:11:44 +0300

    Routing to a hero detail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,46 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {RouterModule}   from '@angular/router';
 
-import { AppComponent } from './app.component';
+import {AppComponent}        from './app.component';
+import {HeroDetailComponent} from './hero-detail.component';
+import {HeroesComponent}     from './heroes.component';
+import {HeroService}         from './hero.service';
+import {DashboardComponent} from "./dashboard.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpModule
+    FormsModule, HttpModule,
+    RouterModule.forRoot([
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      },
+      // {
+      //   path: 'dashboard',
+      //   component: DashboardComponent
+      // },
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      },
+    ])
   ],
-  providers: [],
+  declarations: [
+    AppComponent,
+    HeroDetailComponent,
+    HeroesComponent,
+    DashboardComponent
+  ],
+  providers: [
+    HeroService
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
+
---
+++ src/app/heroes.component.ts
@@ -0,0 +1,34 @@
+import {Component} from '@angular/core';
+import {Hero} from "./hero";
+import {HeroService} from './hero.service';
+
+
+@Component({
+  selector: 'my-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css'],
+  providers: [HeroService]
+})
+export class HeroesComponent {
+
+  heroes: Hero[];
+  selectedHero: Hero;
+
+  constructor(private heroService: HeroService) {
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+}
+
+
+
